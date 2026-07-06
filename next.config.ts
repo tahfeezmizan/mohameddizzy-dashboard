ac555f125d4eb6a566f64c59d443e31b2368d236
Appon Islam <appon2003@gmail.com>
2026-07-07 03:41:57 +0600

    chore: update next.config.ts to allow local IP images and add localhost/127.0.0.1 remote patterns

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,28 @@ export const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
     allowedDevOrigins: ["10.10.7.24", "djarnadash.apponislam.top"],
     images: {
+        dangerouslyAllowLocalIP: true,
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "djarna.apponislam.top",
+                pathname: "/**",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "5050",
+                pathname: "/**",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+                pathname: "/**",
+            },
+            {
+                protocol: "http",
+                hostname: "127.0.0.1",
+                pathname: "/**",
             },
         ],
     },
